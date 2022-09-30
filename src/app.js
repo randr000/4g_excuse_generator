2548dc76d98470a9cf1261e0d867d5658990d1e4
Raul Andrial <rauljandrial@gmail.com>
2022-09-30 02:25:13 +0000

    Initial commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getRandomWord(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = `${getRandomWord(who)} ${getRandomWord(
+    action
+  )} ${getRandomWord(what)} ${getRandomWord(when)}`;
+  document.getElementById("excuse").innerText = excuse;
 };
